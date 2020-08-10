d76797db51ae6b287eb27d85dd9bf530d0a0f156
Sothea Nim <sothea.nim@ezpizee.com>
2020-08-10 03:04:20 -0400

    added lang

--- bundle/src/main/java/com/ezpizee/aem/models/BaseModel.java
+++ bundle/src/main/java/com/ezpizee/aem/models/BaseModel.java
@@ -5,6 +5,7 @@ import com.adobe.cq.sightly.WCMUsePojo;
 import com.ezpizee.aem.Constants;
 import com.ezpizee.aem.services.AdminService;
 import com.ezpizee.aem.utils.CookieUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ public class BaseModel extends WCMUsePojo {
 
     protected final Logger logger = LoggerFactory.getLogger(getClass());
     protected ResourceResolver resolver;
+    protected String lang;
 
     @Override
     public void activate() throws Exception {
@@ -20,6 +22,8 @@ public class BaseModel extends WCMUsePojo {
         if (adminService != null) {
             resolver = adminService.getResourceResolver(Constants.EZPIZEE_SERVICE);
         }
+        String langCode = getRequest().getLocale().toLanguageTag();
+        lang = StringUtils.isNotEmpty(langCode) ? langCode.split("-")[0] : "en";
         exec();
     }
 
