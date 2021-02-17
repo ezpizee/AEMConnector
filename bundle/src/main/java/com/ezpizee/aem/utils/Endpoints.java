a70279ee6c30233df983895c5d98f90807f13065
Sothea Nim <sothea.nim@ezpizee.com>
2021-02-17 11:29:36 +0700

    removed v1 from endpoints

--- bundle/src/main/java/com/ezpizee/aem/utils/Endpoints.java
+++ bundle/src/main/java/com/ezpizee/aem/utils/Endpoints.java
@@ -4,14 +4,14 @@ public class Endpoints {
 
     private Endpoints() {}
 
-    public static String install() {return "/api/v1/install";}
-    public static String token() {return "/api/v1/user/token";}
-    public static String refreshToken(String uuid, String userId) {return "/api/v1/user/token/refresh/"+uuid+"/"+userId;}
-    public static String login() {return "/api/v1/user/login";}
-    public static String logout() {return "/api/v1/user/logout";}
-    public static String signup() {return "/api/v1/user/add";}
-    public static String activateUserWithID(String userId) {return "/api/v1/user/activate/"+userId;}
-    public static String activateUserWithIDAndCode(String userId, String verifyToken) {return "/api/v1/user/activate/"+userId+"/"+verifyToken;}
-    public static String appExists(String clientId, String md5AppName) {return "/api/v1/install/"+clientId+"/"+md5AppName;}
-    public static String appExistsWithStoreID(String clientId, String md5AppName, String storeId) {return "/api/v1/install/"+clientId+"/"+md5AppName+"/"+storeId;}
+    public static String install() {return "/api/install";}
+    public static String token() {return "/api/user/token";}
+    public static String refreshToken(String uuid, String userId) {return "/api/user/token/refresh/"+uuid+"/"+userId;}
+    public static String login() {return "/api/user/login";}
+    public static String logout() {return "/api/user/logout";}
+    public static String signup() {return "/api/user/add";}
+    public static String activateUserWithID(String userId) {return "/api/user/activate/"+userId;}
+    public static String activateUserWithIDAndCode(String userId, String verifyToken) {return "/api/user/activate/"+userId+"/"+verifyToken;}
+    public static String appExists(String clientId, String md5AppName) {return "/api/install/"+clientId+"/"+md5AppName;}
+    public static String appExistsWithStoreID(String clientId, String md5AppName, String storeId) {return "/api/install/"+clientId+"/"+md5AppName+"/"+storeId;}
 }
